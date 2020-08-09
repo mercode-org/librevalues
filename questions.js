aa21f78d35757c34cd7af334a561950a4fb52697
yutyo <tansiretakinci@yandex.com>
2020-08-09 18:41:58 +0300

    initial

---
+++ questions.js
@@ -0,0 +1,123 @@
+questions = [
+    {
+        "question": "The source code of a software should be publicly available",
+        "effect": {
+            "free": 10,
+            "prot": 0,
+            "comm": 0,
+            "attr": 0
+        }
+    },
+
+    {
+        "question": "A software needs to be free to use, modify, redistribute and improve",
+        "effect": {
+            "free": 10,
+            "prot": 0,
+            "comm": 0,
+            "attr": 0
+        }
+    },
+    {
+        "question": "A product is better when its paid",
+        "effect": {
+            "free": 0,
+            "prot": 0,
+            "comm": -10,
+            "attr": 0
+        }
+    },
+    {
+        "question": "A software isn't a product.",
+        "effect": {
+            "free": 0,
+            "prot": 0,
+            "comm": 10,
+            "attr": 0
+        }
+    },
+    {
+        "question": "The freedoms of a free software needs to be preserved",
+        "effect": {
+            "free": 0,
+            "prot": -10,
+            "comm": 0,
+            "attr": 0
+        }
+    },  
+    {
+        "question": "A software isn't free when it has restrictions",
+        "effect": {
+            "free": 0,
+            "prot": 10,
+            "comm": 0,
+            "attr": 0
+        }
+    },
+    {
+        "question": "Attribution doesn't matter",
+        "effect": {
+            "free": 10,
+            "prot": 0,
+            "comm": 0,
+            "attr": -10
+        }
+    },
+    {
+        "question": "Whoever makes a software needs to own it",
+        "effect": {
+            "free": -10,
+            "prot": 0,
+            "comm": 0,
+            "attr": 10
+        }
+    },
+    {
+        "question": "It doesn't matter if someone redistribute my software as proprietary as long as they provide attribution",
+        "effect": {
+            "free": 0,
+            "prot": 10,
+            "comm": 0,
+            "attr": 10
+        }
+    },
+    {
+        "question": "Commercialization of a software matters for its sustainability",
+        "effect": {
+            "free": 0,
+            "prot": 0,
+            "comm": -10,
+            "attr": 0
+        }
+    },
+    {
+        "question": "An intellectual work is valuable when its preserved with property rights",
+        "effect": {
+            "free": -10,
+            "prot": 0,
+            "comm": 0,
+            "attr": 0
+        }
+    },
+    {
+        "question": "Dual licensing a free software with commercial licenses is the rational thing to do",
+        "effect": {
+            "free": -10,
+            "prot": 0,
+            "comm": -10,
+            "attr": 0
+        }
+    },
+    {
+        "question": "A software can be only free when there is no condition at all",
+        "effect": {
+            "free": 0,
+            "prot": 0,
+            "comm": 0,
+            "attr": 10
+        }
+    },
+
+    
+
+];
